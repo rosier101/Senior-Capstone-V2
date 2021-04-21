90879bf3e208d3ae795f85303b1916ebe888cc6f
rosier101 <rosier101@gmail.com>
2021-04-20 20:17:36 -0400

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,7 +30,6 @@ function Footer() {
           <div class='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
             <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
@@ -39,7 +38,6 @@ function Footer() {
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
             <Link to='/'>Sponsorships</Link>
           </div>
         </div>
