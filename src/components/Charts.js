5d5d4824f92c50adab5ce2d2bba7fb01d1005600
rosier101 <rosier101@gmail.com>
2021-04-28 11:54:09 -0400

    changed chart with backend API

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,65 +1,61 @@
-import React, { useState, useEffect } from "react";
-import { Line, Pie } from "react-chartjs-2";
-import axios from "axios";
+import React, {Component} from 'react';
+import {Pie, Bar} from 'react-chartjs-2'
+import axios from 'axios';
+import PostData from '../data/posts.json';
 
-const Charts = () => {
-  const [chartData, setChartData] = useState({});
-  //const [handle, setHandle] = useState([]);
- 
+//const handle = PostData.handle; 
 
-  const chart = () => {
-    let empSal = [];
-    let empAge = [];
-    axios
-      .get("http://dummy.restapiexample.com/api/v1/employees")
+axios
+      .get("https://ig-scraper-senior.herokuapp.com/search?celebhandle=@jakepaul")
       .then(res => {
         console.log(res);
-        for (const dataObj of res.data.data) {
-          empSal.push(parseInt(dataObj.employee_salary));
-          empAge.push(parseInt(dataObj.employee_age));
-        }
-        setChartData({
-          labels: empAge,
-          datasets: [
-            {
-              label: "percentage",
-              data: empSal,
-              backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-              borderWidth: 4
-            }
-          ]
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    console.log(empSal, empAge);
-  };
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 
-  useEffect(() => {
-    chart();
-  }, []);
-  return (
-    <div className="App">
-      <h1>Users</h1>
-      <div>
-        <Line
-          data={chartData}
-          width={500}
+    const Charts = {
+    labels: ['Positive', 'Negative'],
+    datasets: [
+      {
+        label: 'User Comments',
+        backgroundColor: [
+          '#36ff3c',
+          '#C9DE00'
+        ],
+        hoverBackgroundColor: [
+        '#166918',
+        '#4B5000'
+        ],
+        data: [65, 59]
+      }
+    ]
+  }
+
+class Chart extends Component{
+    render() {
+        return (
+          <div>
+            <Pie
+              data={Charts}
+              width={500}
 	          height={500}
               options={{
                 maintainAspectRatio: false,
-            responsive: true,
-            title: { text: "SCALE", display: true },
-            
-                
-              
-            
-          }}
-        />
-      </div>
-    </div>
-  );
-};
+                title:{
+                  display:true,
+                  text:'Average Rainfall per month',
+                  fontSize:20
+                },
+                legend:{
+                  display:true,
+                  position:'right'
+                }
+              }}
+            />
+          </div>
+        );
+      }
+    }
 
-export default Charts;
+export default Chart;
